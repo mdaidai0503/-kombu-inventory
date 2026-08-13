f4e05a0bfdf1da8139ecdf154e29a83348f12882
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-13 22:16:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kombu-inventory-v5';
+const CACHE='kombu-inventory-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./app.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
