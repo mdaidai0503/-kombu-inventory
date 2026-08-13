cccb33b5e40888e4ea335b431de6e99e3452e388
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-14 08:16:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const CACHE='kombu-v35';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./app.js?v=35'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE && (k.startsWith('kombu-')||k.startsWith('kombu-inventory-'))).map(k=>caches.delete(k)))),
-  self.clients.claim()
-])));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  const u=new URL(e.request.url);
-  if(u.origin!==self.location.origin){e.respondWith(fetch(e.request));return;}
-  const isCore=u.pathname.endsWith('/index.html')||u.pathname.endsWith('/app.js')||u.pathname.endsWith('/');
-  if(isCore){
-    e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res})));
-});
+const CACHE='kombu-v37-disabled';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))),self.clients.claim()])));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request));});
