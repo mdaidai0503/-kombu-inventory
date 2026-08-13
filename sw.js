b9f8838a6e2c5860f8556b3323489eb3f37c8fd1
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-14 00:18:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kombu-inventory-v12-shipment-year-stock';
+const CACHE='kombu-inventory-v13-shipment-pdf-blank-zero';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./app.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
