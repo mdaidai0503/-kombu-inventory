e18cab31e3680c63243924de4112a509dd03d6c4
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-29 18:14:13 +0900

    Update sw-v160.js

--- sw-v160.js
+++ sw-v160.js
@@ -1,9 +1,9 @@
-const CACHE = 'kombu-inventory-v162-1';
+const CACHE = 'kombu-inventory-v162-2';
 
 const APP_SHELL = [
   './',
   './index.html',
-  './app-v159.js?v=162.1',
+  './app-v159.js?v=162.2',
   './manifest.webmanifest',
   './icon-180.png',
   './pdf-v58.js',
